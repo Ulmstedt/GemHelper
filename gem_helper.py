6a554df2c2bb643f8c1841ff81068362a0942782
Ulmstedt <matte___93@hotmail.com>
2016-06-12 16:55:21 +0200

    Format code

--- gem_helper.py
+++ gem_helper.py
@@ -25,13 +25,14 @@ class GemHelper:
 
         tower3_frame = tk.LabelFrame(mandatory_frame)
         self.tower_list.append(Tower(tower3_frame, "Paraiba Tourmaline",
-              ["Flawed Aquamarine", "Flawed Emerald", "Flawless Opal", "Perfect Aquamarine"]))
+                                     ["Flawed Aquamarine", "Flawed Emerald", "Flawless Opal", "Perfect Aquamarine"]))
 
         tower4_frame = tk.LabelFrame(mandatory_frame)
         self.tower_list.append(Tower(tower4_frame, "Jade", ["Flawed Sapphire", "Opal", "Flawless Emerald"]))
 
         tower5_frame = tk.LabelFrame(mandatory_frame)
-        self.tower_list.append(Tower(tower5_frame, "Dark Emerald", ["Flawed Topaz", "Flawless Sapphire", "Perfect Emerald"]))
+        self.tower_list.append(
+            Tower(tower5_frame, "Dark Emerald", ["Flawed Topaz", "Flawless Sapphire", "Perfect Emerald"]))
 
         tower6_frame = tk.LabelFrame(mandatory_frame)
         self.tower_list.append(Tower(tower6_frame, "Pink Diamond", ["Topaz", "Diamond", "Perfect Diamond"]))
@@ -43,7 +44,8 @@ class GemHelper:
         self.tower_list.append(Tower(tower8_frame, "Uranium", ["Flawed Opal", "Sapphire", "Perfect Topaz"]))
 
         tower9_frame = tk.LabelFrame(situational_frame)
-        self.tower_list.append(Tower(tower9_frame, "Yellow Sapphire", ["Flawless Ruby", "Flawless Topaz", "Perfect Sapphire"]))
+        self.tower_list.append(
+            Tower(tower9_frame, "Yellow Sapphire", ["Flawless Ruby", "Flawless Topaz", "Perfect Sapphire"]))
 
         tower10_frame = tk.LabelFrame(situational_frame)
         self.tower_list.append(Tower(tower10_frame, "Blood Stone", ["Amethyst", "Flawless Aquamarine", "Perfect Ruby"]))
@@ -52,7 +54,8 @@ class GemHelper:
         self.tower_list.append(Tower(tower11_frame, "Star Ruby", ["Chipped Ruby", "Flawed Ruby", "Chipped Amethyst"]))
 
         tower12_frame = tk.LabelFrame(situational_frame)
-        self.tower_list.append(Tower(tower12_frame, "Malachite", ["Chipped Aquamarine", "Chipped Opal", "Chipped Emerald"]))
+        self.tower_list.append(
+            Tower(tower12_frame, "Malachite", ["Chipped Aquamarine", "Chipped Opal", "Chipped Emerald"]))
 
         # Add to layout
         TOWER_PADX = 8
@@ -71,7 +74,8 @@ class GemHelper:
         tower12_frame.grid(row=2, column=3, padx=TOWER_PADX, pady=TOWER_PADY, sticky=tk.N)
 
         # Clear button
-        tk.Button(situational_frame, text="Clear all", command=self.reset_all).grid(row=3, column=3, sticky=tk.E)
+        tk.Button(situational_frame, text="Clear all", command=self.reset_all).grid(row=3, column=3, sticky=tk.E,
+                                                                                    padx=9)
 
         mandatory_frame.grid(row=0, column=0, padx=3, pady=3)
         situational_frame.grid(row=0, column=1, padx=3, pady=3)
